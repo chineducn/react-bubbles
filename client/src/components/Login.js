4e7387e507f0b4b373feaec0d20c8ddf7d076d28
unknown <neduvinci@gmail.com>
2019-10-18 08:29:25 +0100

    Created basic login structure

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Login = () => {
+  const usernmeRef = useRef();
+  const passwordRef = useRef();
+
+  const onLogin = () => {
+    debugger
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <label htmlFor="username">
+          Username
+          <input
+            type="text"
+            id='username'
+            ref={usernmeRef}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="text"
+            id='password'
+            ref={passwordRef}
+          />
+        </label>
+        <button onClick={onLogin}>Login</button>
+      </div>
     </>
   );
 };
